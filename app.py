d61b81a5e8a60b584e0a53ff1db3a9364f1e4ac8
powersystems013 <111869688+powersystems013@users.noreply.github.com>
2024-06-11 16:14:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app = FastAPI()
  
 @app.get("/")
 def read_root():
-    return {"Hello": "jenkins_cicd_pipeline"}
+ns    return {"Hello": "Jenkins CI/CD Pipeline Setup for Web App Deployment on AWS EKS Cluster"}
  
 handler = mangum.Mangum(app)
  
