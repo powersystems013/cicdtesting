45c0c2c49799833f1c193836fd0cd25da121bbed
powersystems013 <111869688+powersystems013@users.noreply.github.com>
2024-06-11 15:52:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+#from flask import Flask
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello, World!"
+#@app.route('/')
+#def hello_world():
+#   return 'Hello Jenkins webhookss'
 
+#if __name__ == '__main__':
+ #   app.run(debug=True)
+
+#
+
+from fastapi import FastAPI
+import mangum
+import uvicorn
+ 
+app = FastAPI()
+ 
+@app.get("/")
+def read_root():
+    return {"Hello": "jenkins_cicd_pipeline"}
+ 
+handler = mangum.Mangum(app)
+ 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+   uvicorn.run(app, host="0.0.0.0", port=8080)
